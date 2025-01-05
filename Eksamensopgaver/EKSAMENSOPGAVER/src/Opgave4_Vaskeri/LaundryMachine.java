1642a1071a5f12ea4630fce2d96f7aa1ebd994b1
V2S2P <157969468+V2S2P@users.noreply.github.com>
2025-01-05 17:00:37 +0100

    Delete Eksamensopgaver directory

--- Eksamensopgaver/EKSAMENSOPGAVER/src/Opgave4_Vaskeri/LaundryMachine.java
+++
@@ -1,8 +0,0 @@
-package Opgave4_Vaskeri;
-
-public interface LaundryMachine {
-    public void chooseProgram();
-    public void displayPriceAndDuration();
-    public void start();
-    public void addExtra();
-}
